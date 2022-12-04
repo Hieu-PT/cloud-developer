8272a481908ea83ac07d1507752567f7aad8d0d0
Hieu-PT <HieuPT.Work@gmail.com>
2022-12-04 19:29:47 +0700

    update

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://937g6c0vyc.execute-api.us-east-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-qs3nc445ohyo15ed.us.auth0.com',
+  clientId: '7s0eBsuebQzewrZyU1sBnquCl0v5VRz2',
   callbackUrl: 'http://localhost:3000/callback'
 }
